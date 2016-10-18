c63b82c05dd6a4e025204e4c9abc76066cd87773
Kyle Smith <askreet@gmail.com>
2016-10-18 18:00:36 -0400

    Fixes #155: Remove guesses about update changes from status output. (#157)

--- lib/moonshot/stack_parameter_printer.rb
+++ lib/moonshot/stack_parameter_printer.rb
@@ -9,62 +9,16 @@ module Moonshot
 
     def print
       p_table = @table.add_leaf('Stack Parameters')
-      overrides = @stack.overrides
       rows = @stack.parameters.sort.map do |key, value|
-        t_param = @stack.template.parameters.find do |p|
-          p.name == key
-        end
-
-        properties = determine_change(t_param ? t_param.default : nil,
-                                      overrides[key],
-                                      value)
-
-        [
-          "#{key}:",
-          format_value(value),
-          format_properties(properties)
-        ]
+        ["#{key}:", format_value(value)]
       end
 
       p_table.add_table(rows)
     end
 
-    def determine_change(default, override, current)
-      properties = []
-
-      # If there is a stack override, determine if it would change the current
-      # stack.
-      if override
-        properties << 'overridden'
-        if current == '****'
-          properties << 'may be updated, NoEcho set'
-        elsif override != current
-          properties << "would be updated to #{override}"
-        end
-
-      else
-        # Otherwise, compare the template default with the current value to
-        # determine outcome.
-        properties << 'default'
-        properties << "would be updated to #{default}" if default != current
-      end
-
-      properties
-    end
-
-    def format_properties(properties)
-      string = " (#{properties.join(', ')})"
-
-      if properties.any? { |p| p =~ /be updated/ }
-        string.yellow
-      else
-        string.green
-      end
-    end
-
     def format_value(value)
-      if value.size > 40
-        value[0..40] + '...'
+      if value.size > 60
+        value[0..60] + '...'
       else
         value
       end
