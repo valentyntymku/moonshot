87dc0fd3f87286f3379c589e45acb85088f08cd1
Gergely Brautigam <skarlso777@gmail.com>
2016-12-16 22:41:43 +0100

    Fixing namespacing for kms_key and parameter_encrypter. #190 (#191)

--- lib/plugins/encrypted_parameters/kms_key.rb
+++ lib/plugins/encrypted_parameters/kms_key.rb
@@ -1,22 +1,24 @@
-class EncryptedParameters
-  # Class that manages KMS keys in AWS.
-  class KmsKey
-    attr_reader :arn
+module Moonshot
+  class EncryptedParameters
+    # Class that manages KMS keys in AWS.
+    class KmsKey
+      attr_reader :arn
 
-    def initialize(arn)
-      @arn = arn
-      @kms_client = Aws::KMS::Client.new
-    end
+      def initialize(arn)
+        @arn = arn
+        @kms_client = Aws::KMS::Client.new
+      end
 
-    def self.create
-      resp = Aws::KMS::Client.new.create_key
-      arn = resp.key_metadata.arn
+      def self.create
+        resp = Aws::KMS::Client.new.create_key
+        arn = resp.key_metadata.arn
 
-      new(arn)
-    end
+        new(arn)
+      end
 
-    def delete
-      @kms_client.schedule_key_deletion(key_id: @arn, pending_window_in_days: 7)
+      def delete
+        @kms_client.schedule_key_deletion(key_id: @arn, pending_window_in_days: 7)
+      end
     end
   end
 end
--- lib/plugins/encrypted_parameters/parameter_encrypter.rb
+++ lib/plugins/encrypted_parameters/parameter_encrypter.rb
@@ -1,23 +1,24 @@
 require 'base64'
+module Moonshot
+  class EncryptedParameters
+    # Class that can encrypt and decrypt parameters using KMS.
+    class ParameterEncrypter
+      # @param [String] key_arn The ARN for the KMS key.
+      def initialize(key_arn)
+        @kms_client = Aws::KMS::Client.new
+        @key_arn = key_arn
+      end
 
-class EncryptedParameters
-  # Class that can encrypt and decrypt parameters using KMS.
-  class ParameterEncrypter
-    # @param [String] key_arn The ARN for the KMS key.
-    def initialize(key_arn)
-      @kms_client = Aws::KMS::Client.new
-      @key_arn = key_arn
-    end
+      # Encrypt and base64 encode the parameter value.
+      #
+      # @param [String] param_value The parameter to encrypt.
+      # @return [String] base64 encoded encrypted ciphertext.
+      def encrypt(param_value)
+        resp = @kms_client.encrypt(key_id: @key_arn, plaintext: param_value)
 
-    # Encrypt and base64 encode the parameter value.
-    #
-    # @param [String] param_value The parameter to encrypt.
-    # @return [String] base64 encoded encrypted ciphertext.
-    def encrypt(param_value)
-      resp = @kms_client.encrypt(key_id: @key_arn, plaintext: param_value)
-
-      # Use strict here to avoid newlines which cause issues with parameters.
-      Base64.strict_encode64(resp.ciphertext_blob)
+        # Use strict here to avoid newlines which cause issues with parameters.
+        Base64.strict_encode64(resp.ciphertext_blob)
+      end
     end
   end
 end
