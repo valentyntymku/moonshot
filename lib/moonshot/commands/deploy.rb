cb065514b825bc1e03217204380708f10b18666a
Peter Drake <peter.drake@acquia.com>
2017-06-13 16:27:08 -0400

    Add interactive / no-interactive option to the deploy command.

--- lib/moonshot/commands/deploy.rb
+++ lib/moonshot/commands/deploy.rb
@@ -4,6 +4,14 @@ module Moonshot
       self.usage = 'deploy VERSION'
       self.description = 'Deploy a versioned release to the environment'
 
+      def parser
+        parser = super
+
+        parser.on('--[no-]interactive', TrueClass, 'Use interactive prompts.') do |v|
+          Moonshot.config.interactive = v
+        end
+      end
+
       def execute(version_name)
         controller.deploy_version(version_name)
       end
