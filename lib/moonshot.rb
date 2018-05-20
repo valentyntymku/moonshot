abee859d2fff66bffcc95facd57fc11fb9169a7a
Kyle Smith <kyle.smith@acquia.com>
2018-05-20 19:34:50 -0400

    Update to AWS-SDK v3 gems, use waiter for ChangeSets.

--- lib/moonshot.rb
+++ lib/moonshot.rb
@@ -1,5 +1,12 @@
 require 'English'
-require 'aws-sdk'
+
+require 'aws-sdk-cloudformation'
+require 'aws-sdk-codedeploy'
+require 'aws-sdk-ec2'
+require 'aws-sdk-iam'
+require 'aws-sdk-autoscaling'
+require 'aws-sdk-s3'
+
 require 'logger'
 require 'thor'
 require 'interactive-logger'
