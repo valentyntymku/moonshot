06c3bd6329fbaae597ee56a590b4a5ae3f070fdc
Valentyn Tymku <valentyn.tymku@gmail.com>
2019-07-29 14:53:31 +0000

    upd

--- lib/plugins/dynamic_template.rb
+++ lib/plugins/dynamic_template.rb
@@ -22,8 +22,8 @@ module Moonshot
       end
 
       # Moonshot hooks to trigger this plugin.
-      alias pre_create run_hook
-      alias pre_update run_hook
+      alias post_cli run_hook
+#      alias pre_update run_hook
 #      alias setup_create run_hook
 #      alias setup_update run_hook
     end
